dbf64084332c8fde5e0a9df867d836b1366b0f77
Agnès Regaud <agnes.regaud@yahoo.com>
2023-12-08 11:56:39 +0100

    annulation test pytest

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,5 +12,5 @@ def test_index(app, client):
     
     res = client.get('/')
     assert res.status_code == 200
-    expected = {'hello': 'juliette'}
+    expected = {'hello': 'world'}
     assert expected == json.loads(res.get_data(as_text=True))
